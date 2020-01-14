f76169a7ef2343cdfc92ee5eaa16fc9249bb358e
Matthew Dillon <matthewrdillon@gmail.com>
2020-01-14 16:53:43 -0700

    MAINT: Rough in rendered docs

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 aws s3 cp \
-    build/html/data s3://qiime2-curr-protoc-bioinformatics/ \
+    build/html/data s3://qiime2-curr-protoc-bioinformatics/data \
     --recursive \
     --acl 'public-read' && \
 cp -r build/html/* docs/
